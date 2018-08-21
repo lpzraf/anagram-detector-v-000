d6e6eaabad38fb561f623c0541e350cf486c9315
Rafael Lopez <lpz.raf@gmail.com>
2018-08-21 01:56:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,12 +8,8 @@ class Anagram
   end
     
   def match(possible_anagrams)
-    no_matches = []
     possible_anagrams.select do |anagram|
      anagram.split("").sort == word.split("").sort
-
-  end
-  no_matches
 end
 end
 end
